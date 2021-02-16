03f67bb52f1e8da28f735b5a5801169449af62d6
jose andres <contretaa@gmail.com>
2021-02-16 14:05:10 -0400

    nav user finished

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,20 +1,29 @@
 import './style.scss'
 
-export default function Button({text, modifier, onClick}){
+export default function Button({text, modifier, onClick, icon}){
     if(Array.isArray(modifier)){
         let nameClassBasic = 'button'
         modifier.forEach(element => nameClassBasic+=` button--${element}`)
         return (
             <button
                 className={nameClassBasic} 
-            >{text}</button>
+            >
+                {icon}
+                {text}
+            </button>
         )
     }
-    if (!modifier) return <button className="button">{text}</button>
+    if (!modifier) return <button className="button">
+        {icon}
+        {text}
+    </button>
 
     return (
         <button 
         onClick={onClick}
-        className={`button button--${modifier}`}>{text}</button>
+        className={`button button--${modifier}`}>
+            {icon}
+            {text}
+        </button>
     )
 }
--- src/components/NavUser/index.js
+++ src/components/NavUser/index.js
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faFileUpload } from '@fortawesome/free-solid-svg-icons'
+
+import Button from '../Button'
 import Search from "../Search";
 
 import './style.scss'
@@ -5,6 +9,8 @@ export default function NavUser({}){
     return (
        <nav className="nav-user">
            <Search/>
+           <Button text="Upload" modifier="hidden-min-width" icon={<FontAwesomeIcon icon={faFileUpload} className="icon"/>}/>
+           <Button icon={<FontAwesomeIcon icon={faUser}/>} modifier="hidden-min-width"/>
        </nav> 
     )
 }
