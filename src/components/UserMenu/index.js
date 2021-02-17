b2e420826acb26d9bee348cd9e2f2ce3305f9245
jose andres <contretaa@gmail.com>
2021-02-16 21:10:06 -0400

    modifing user menu

---
+++ src/components/UserMenu/index.js
@@ -0,0 +1,10 @@
+import './style.scss'
+
+export default function UserMenu(){
+
+    return(
+        <div className="menu-user">
+            <h2 className="menu-user__title">Hi User !</h2>
+        </div>
+    )
+}
